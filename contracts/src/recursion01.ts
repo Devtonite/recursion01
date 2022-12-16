882a40340fa9e19b5d8658b143a5de207e038de7
Devtonite <devtonite001@gmail.com>
2022-12-16 09:51:10 -0600

    Add potential feature that uses Poseidon hashes

--- contracts/src/recursion01.ts
+++ contracts/src/recursion01.ts
@@ -1,24 +1,37 @@
-import { Experimental, Int64, SelfProof } from 'snarkyjs';
+import { Experimental, Field, Int64, Poseidon, Struct } from 'snarkyjs';
 
 export const SimpleProgram = Experimental.ZkProgram({
   publicInput: Int64,
 
   methods: {
-    base: {
-      privateInputs: [],
+    // base: {
+    //   privateInputs: [],
 
-      method(publicInput: Int64) {
-        publicInput.assertEquals(Int64.from(0));
-      },
-    },
+    //   method(publicInput: Int64) {
+    //     publicInput.assertEquals(Int64.from(0));
+    //   },
+    // },
 
-    step: {
-      privateInputs: [SelfProof],
+    // step: {
+    //   privateInputs: [SelfProof],
 
-      method(publicInput: Int64, earlierProof: SelfProof<Int64>) {
-        earlierProof.verify();
-        earlierProof.publicInput.add(2).assertEquals(publicInput);
-      },
+    //   method(publicInput: Int64, earlierProof: SelfProof<Int64>) {
+    //     earlierProof.verify();
+    //     earlierProof.publicInput.add(2).assertEquals(publicInput);
+    //   },
+    // },
+
+    init: {
+      privateInputs: [answer],
     },
   },
 });
+
+class answer extends Struct({
+  salt: Field,
+  numToWin: Int64,
+}) {}
+
+export function hashItbruh(input: answer) {
+  return Poseidon.hash([input.salt, input.numToWin.toField()]);
+}
--- contracts/src/run-recursion01.ts
+++ contracts/src/run-recursion01.ts
@@ -1,4 +1,4 @@
-import { Int64, isReady, shutdown } from 'snarkyjs';
+import { Field, Int64, isReady, Poseidon, shutdown } from 'snarkyjs';
 import { SimpleProgram } from './recursion01.js';
 
 (async function main() {
@@ -10,15 +10,18 @@ import { SimpleProgram } from './recursion01.js';
   console.log('Compiled and verification key generated:');
   console.log(verificationKey.toString());
 
-  // 1st user Field input
-  const proof0 = await SimpleProgram.base(Int64.from(0));
-  console.log('Proof generated:');
-  console.log(proof0.toJSON());
+  // // 1st user Field input
+  // const proof0 = await SimpleProgram.base(Int64.from(0));
+  // console.log('Proof generated:');
+  // console.log(proof0.toJSON());
 
-  // 2nd user Field input
-  const proof1 = await SimpleProgram.step(Int64.from(2), proof0);
-  console.log('Proof generated:');
-  console.log(proof1.toJSON());
+  // // 2nd user Field input
+  // const proof1 = await SimpleProgram.step(Int64.from(2), proof0);
+  // console.log('Proof generated:');
+  // console.log(proof1.toJSON());
+
+  const answer = Poseidon.hash([Field('mysalt'), Int64.from(75).toField()]);
+  console.log(answer);
 
   await shutdown();
 })();
