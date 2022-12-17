0910ec2622b666f20773f119007e05e408539f9b
Eltonite <eltoniteonblockchain@gmail.com>
2022-12-16 23:14:55 -0600

    new beginnings maybe?

--- contracts/src/recursion01.ts
+++ contracts/src/recursion01.ts
@@ -1,24 +1,56 @@
-import { Experimental, Field, Poseidon, SelfProof } from 'snarkyjs';
+import { Experimental, Field, Poseidon, Struct, UInt32 } from 'snarkyjs';
+
+export class gameState extends Struct({
+  solutionHash: Field,
+  lastGuess: Field,
+  numTurns: UInt32,
+}) {}
 
 export const SimpleProgram = Experimental.ZkProgram({
-  publicInput: Field,
+  publicInput: gameState,
 
   methods: {
-    base: {
-      privateInputs: [Field],
+    initState: {
+      privateInputs: [Field, Field],
 
-      method(publicInput: Field, guessToWin: Field) {
-        return Poseidon.hash([publicInput, guessToWin]);
+      method(publicInput: gameState, salt: Field, winningNum: Field) {
+        publicInput.solutionHash = Poseidon.hash([salt, winningNum]);
+        publicInput.lastGuess = Field(-1);
+        publicInput.numTurns = UInt32.from(0);
       },
     },
 
-    step: {
-      privateInputs: [SelfProof],
+    // base: {
+    //   privateInputs: [Field],
 
-      method(publicInput: Field, earlierProof: SelfProof<Field>) {
-        earlierProof.verify();
-        earlierProof.publicInput.add(2).assertEquals(publicInput);
-      },
-    },
+    //   method(publicInput: Field, guessToWin: Field) {
+    //     const valHere = publicInput
+    //     publicInput.assertEquals(0)
+    //     guessToWin.assertGte(0)
+    //     guessToWin.assertLte(100)
+
+    //     publicInput = valHere.add(1)
+    //     publicInput.
+    //   },
+    // },
+
+    // step: {
+    //   privateInputs: [SelfProof],
+
+    //   method(publicInput: Field, earlierProof: SelfProof<Field>) {
+    //     earlierProof.verify();
+    //     earlierProof.publicInput.add(2).assertEquals(publicInput);
+    //   },
+    // },
+
+    // checkGuess: {
+    //   privateInputs: [Field,SelfProof],
+
+    //   method(publicInput: Field, myGuess: Field, earlierProof: SelfProof<Field>){
+    //     publicInput.assertEquals(publicInput)
+    //     const valHere = publicInput
+    //     publicInput = valHere.add(1)
+    //   }
+    // }
   },
 });
--- contracts/src/run-recursion01.ts
+++ contracts/src/run-recursion01.ts
@@ -1,5 +1,5 @@
-import { Field, isReady, shutdown } from 'snarkyjs';
-import { SimpleProgram } from './recursion01.js';
+import { Field, UInt32, isReady, shutdown } from 'snarkyjs';
+import { SimpleProgram, gameState } from './recursion01.js';
 import { tic, toc } from './tictoc.js';
 
 (async function main() {
@@ -8,21 +8,39 @@ import { tic, toc } from './tictoc.js';
   toc();
 
   tic('Compiling');
-  const { verificationKey } = await SimpleProgram.compile();
+  await SimpleProgram.compile();
   toc();
+  // console.log('Compiled and verification key generated:');
+  // console.log(verificationKey.toString());
 
-  console.log('Compiled and verification key generated:');
-  console.log(verificationKey.toString());
+  let winState = new gameState({
+    solutionHash: Field(0),
+    lastGuess: Field(-1),
+    numTurns: UInt32.from(0),
+  });
+
+  console.log(winState.solutionHash.toJSON());
+  tic('proof0');
+  // const proof0 =
+  await SimpleProgram.initState(winState, Field(10), Field(75));
+  toc();
+  console.log(winState.solutionHash.toJSON());
 
   // 1st user Field input
-  tic('proof0');
-  const proof0 = await SimpleProgram.base(Field(0), Field(75));
-  toc();
-  console.log('Proof generated:');
-  console.log(proof0.toJSON());
+  // tic('proof0');
+  // const proof0 = await SimpleProgram.base(Field(0), Field(90));
+  // toc();
+  // console.log('Proof generated:');
+  // console.log(proof0.toJSON());
 
   // 2nd user Field input
   // const proof1 = await SimpleProgram.step(Int64.from(2), proof0);
+  // console.log('Proof generated:');
+  // console.log(proof1.toJSON());
+
+  // tic('proof1');
+  // const proof1 = await SimpleProgram.checkGuess(Field(2), Field(75), proof0);
+  // toc();
   // console.log('Proof generated:');
   // console.log(proof1.toJSON());
 
